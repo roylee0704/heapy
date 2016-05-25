734cfb59022996ce0731e0f87894eeebc7accd20
roylee0704 <roylee0704@gmail.com>
2016-05-25 13:46:06 +0800

    initial commit worker

---
+++ worker/README.md
@@ -0,0 +1,3 @@
+# worker/heapy.go
+
+All goes the same as generic `item/heapy.go`, except for `update()`.
---
+++ worker/heapy.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"container/heap"
+	"flag"
+	"fmt"
+)
+
+var (
+	nWorkers = flag.Int("n", 5, "number of workers")
+)
+
+func main() {
+
+	flag.Parse()
+	b := NewBalancer(*nWorkers)
+
+	for i := 0; i < 10; i++ {
+		w := heap.Pop(&b.pool).(*Worker)
+		w.pending++
+		heap.Push(&b.pool, w)
+
+		b.Print()
+	}
+
+}
+
+// Balancer dispatches jobs to workers, and update upon completion.
+type Balancer struct {
+	pool Pool
+}
+
+// NewBalancer returns new balancers with a pool of n workers.
+func NewBalancer(n int) *Balancer {
+	return &Balancer{genWorker(n)}
+}
+
+// genWorker produces workers based on number to generate.
+func genWorker(n int) Pool {
+	p := make(Pool, 0, n)
+	for i := 0; i < n; i++ {
+		w := &Worker{
+			pending: 0,
+			name:    fmt.Sprintf("[%.2d]", i),
+		}
+		heap.Push(&p, w)
+	}
+	return p
+}
+
+// Worker is something we manage in a pending queue
+type Worker struct {
+	i       int // index is needed by update
+	pending int
+	name    string
+}
+
+// Pool implements heap.Interface and holds pointers to Workers.
+type Pool []*Worker
+
+func (p Pool) Len() int { return len(p) }
+
+func (p Pool) Less(i, j int) bool {
+	return p[i].pending < p[j].pending
+}
+
+func (p *Pool) Swap(i, j int) {
+	a := *p
+	a[i], a[j] = a[j], a[i]
+	a[i].i = i
+	a[j].i = j
+}
+
+func (p *Pool) Push(x interface{}) {
+	a := *p
+	n := len(a)
+	a = a[0 : n+1]
+	w := x.(*Worker)
+	a[n] = w
+	w.i = n
+	*p = a
+}
+
+func (p *Pool) Pop() interface{} {
+	a := *p
+	*p = a[0 : len(a)-1]
+	w := a[len(a)-1]
+	w.i = -1 // for safety
+	return w
+}
+
+func (b Balancer) Print() {
+	var buffer bytes.Buffer
+
+	for _, worker := range b.pool {
+		//fmt.Printf("%+v %d\n", worker, i)
+		buffer.WriteString(fmt.Sprintf("[%d]:%d = %s  ,", worker.i, worker.pending, worker.name))
+	}
+	fmt.Println(buffer.String())
+}
